67d70deda2843de7ca5a5abc663733a8cb65b989
Ansh Verma <102733398+Anshverma2003@users.noreply.github.com>
2022-11-25 01:36:31 +0530

    Added a like and dislike button

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-fetch('https://icanhazdadjoke.com/slack')//get request api
-    .then(data=>data.json())
-    .then(jokeData =>{
-        const jokeText=jokeData.attachments[0].text;
-        const jokeElement=document.getElementById('jokeElement');
-        jokeElement.innerHTML=jokeText;
-    })
+const url = 'https://icanhazdadjoke.com/slack';//url
+fetch(url)//get request api
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById('jokeElement');
+        jokeElement.innerHTML = jokeText;
+    });
+
+// function of like a dislike to call again
+
+function get() {
+    const url = 'https://icanhazdadjoke.com/slack';
+    fetch(url)//get request api
+        .then(data => data.json())
+        .then(jokeData => {
+            const jokeText = jokeData.attachments[0].text;
+            const jokeElement = document.getElementById('jokeElement');
+            jokeElement.innerHTML = jokeText;
+        });
+}
+//query to access like and dislike buttons.
+document.querySelector("i").addEventListener("click", () => {
+    get();
+});
+
+document.querySelector(".dislike").addEventListener("click", () => {
+    get();
+});
